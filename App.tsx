975980393245887196bd657e70315607bba7fd3a
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-06 20:40:27 -0300

    loading custom fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
+/* eslint-disable camelcase */
 import { ThemeProvider } from 'styled-components/native'
 import { Groups } from './src/screens/groups'
 import theme from '@theme/index'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from 'react-native'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <ActivityIndicator />}
     </ThemeProvider>
   )
 }

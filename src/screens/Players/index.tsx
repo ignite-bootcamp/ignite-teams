c215436c3f2d26cff1a2782a9552de245890a52a
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-08 20:26:36 -0300

    PlayerCard component

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,13 +3,14 @@ import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
+import { PlayerCard } from '@components/PlayerCard'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
 import { Container, Form, HeaderList, NumberOfPlayers } from './styles'
 
 export function Players() {
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState([])
+  const [players, setPlayers] = useState(['Rodrigo', 'Vini'])
   return (
     <Container>
       <Header showBackButton />
@@ -41,6 +42,13 @@ export function Players() {
 
         <NumberOfPlayers>{players.length}</NumberOfPlayers>
       </HeaderList>
+      <FlatList
+        data={players}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+      />
     </Container>
   )
 }

b023db5358ede4cac57d89120613102e3134f2fa
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-02-08 21:41:54 -0300

    implementing go back on header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { BackButton, BackIcon, Container, Logo } from './styles'
+import { useNavigation } from '@react-navigation/native'
 
 import logoImg from '@assets/logo.png'
 
@@ -7,10 +8,16 @@ type HeaderProps = {
 }
 
 export function Header({ showBackButton }: HeaderProps) {
+  const navigation = useNavigation()
+
+  function handleGoBack() {
+    navigation.navigate('groups')
+  }
+
   return (
     <Container>
       {showBackButton && (
-        <BackButton>
+        <BackButton onPress={handleGoBack}>
           <BackIcon />
         </BackButton>
       )}
